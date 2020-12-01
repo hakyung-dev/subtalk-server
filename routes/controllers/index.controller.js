d3e39c89f2eb1c9924ccc348e95f8e7fa11e4639
hakyung <hakyung.dev@gmail.com>
2020-12-02 01:36:37 +0900

    Add indexRoute for api

---
+++ routes/controllers/index.controller.js
@@ -0,0 +1,34 @@
+const axios = require('axios');
+
+exports.getNearStation = async (req, res, next) => {
+  try {
+    const location = req.body;
+    const station = await axios.get(
+      `http://swopenAPI.seoul.go.kr/api/subway/${process.env.SUBWAY_STATION_KEY}/json/nearBy/0/10/${location[0]}/${location[1]}`
+    );
+
+    res.status(200).json({
+      result: station.data.stationList,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
+
+exports.getStationInfo = async (req, res, next) => {
+  try {
+    const station = await axios.get(
+      `http://swopenAPI.seoul.go.kr/api/subway/${
+        process.env.SUBWAY_STATION_KEY
+      }/json/stationInfo/0/10/${encodeURI(req.body.stationName)}`
+    );
+
+    res.status(200).json({
+      result: station.data,
+    });
+  } catch (err) {
+    console.log(err);
+    next(err);
+  }
+};
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 
-/* GET home page. */
-router.get('/', function (req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+const indexController = require('./controllers/index.controller');
+
+router.put('/getNearSubwayStation', indexController.getNearStation);
+router.put('/getStationInfo', indexController.getStationInfo);
 
 module.exports = router;
