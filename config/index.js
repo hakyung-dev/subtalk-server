f84d0cd6ad0130746262a6ed66b51a5e0ddfbc53
hakyung <hakyung.dev@gmail.com>
2020-12-04 01:44:50 +0900

    Change CLIENT_URL

--- config/index.js
+++ config/index.js
@@ -1,4 +1,6 @@
 const CLIENT_URL =
-  process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'PRODUCT';
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : 'https://subtalk.devhak.com';
 
 module.exports = { CLIENT_URL };
